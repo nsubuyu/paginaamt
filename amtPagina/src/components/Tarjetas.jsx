8f3ff9aa5ccbf11c1357dd2478926459fe02bf6c
nicky subuyu <137240558+codeflow1958@users.noreply.github.com>
2024-08-21 23:51:03 -0600

    tarsrts

---
+++ amtPagina/src/components/Tarjetas.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Tarjetas(props) {
+  return (
+    <div>
+      <h1></h1>
+      <img src="" alt="" />
+      <p></p>
+    </div>
+  );
+}
+
+export default Tarjetas;
