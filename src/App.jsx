f54a9bc76c4975fdde9282f96e25b30c67970280
nsubuyu <nsubuyu@yetiadvisers.com>
2024-08-27 00:14:37 -0600

    nicky

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import Imagenp from "./components/Imagenp";
 import Tarjet from "./components/Tarjet";
 import AOS from "aos";
 import { useEffect } from "react";
-import "./components/css/tag.css"
-import "./components/css/azul.css"
-import "./components/css/heder.css"
-import "./components/css/icons.css"
-import "./components/css/imagenp.css"
-import "./components/css/parrafo.css"
+import "./components/css/tag.css";
+import "./components/css/azul.css";
+import "./components/css/heder.css";
+import "./components/css/icons.css";
+import "./components/css/imagenp.css";
+import "./components/css/parrafo.css";
 
 
 import img1 from "./assets/pagina amt/PORTA DE INICIO.png";
@@ -40,12 +40,8 @@ import img19 from "./assets/pagina amt/9.png";
 
 
 function App() {
-  
-  useEffect(()=>{ 
-    
-    AOS.init({duration:3000});
-  
-  },[])
+
+
 
   return (
     <>
@@ -55,18 +51,16 @@ function App() {
           alter="imagen"
         />
       </section>
-       <div className="position-relative ">
-       <div data-aos="fade-up" className="text-center amt m-5 ribbon" >
-        <Titulo
-          texto=" AMT CUSTOMS SERVICES"
-          negritas="Importaciones y Exportaciones"
-        />
-      </div>
-    {/*<div className="position-absolute z-n1  atrastexto">
+      <div className="position-relative ">
+        <div data-aos="fade-up" className="text-center amt m-5 ribbon" >
+          <Titulo
+            texto=" AMT CUSTOMS SERVICES"
+            negritas="Importaciones y Exportaciones"
+          />
+        </div>
 
-      </div>*/}
-       </div>
-      
+      </div>
+
       <section>
         <div>
           <div className="texto text-white" >
@@ -81,8 +75,8 @@ function App() {
             />
           </div>
           <div className="fondo">
-            <Header imagen={img2} alter="camion" clase="lineazul"/>
-            
+            <Header imagen={img2} alter="camion" clase="lineazul" />
+
           </div>
         </div>
       </section>
@@ -110,65 +104,65 @@ function App() {
         </p>
       </div>
 
-      <Header imagen={img4} alter="portada amt" clase="lineazul"/>
+      <Header imagen={img4} alter="portada amt" clase="lineazul" />
 
       <section>
         <div className="mv m-5">
           <h3>Misión y Visión</h3>
         </div>
         <div className="mx-5">
-        <div className=" d-flex justify-content-start ">
-        <div className="">
-          <Tags
-            imagen={img5}
-            alter="vision"
-            titulo="VISION"
-            texto="Ser una empresa con vocación de servicio, 
+          <div className=" d-flex justify-content-start ">
+            <div className="">
+              <Tags
+                imagen={img5}
+                alter="vision"
+                titulo="VISION"
+                texto="Ser una empresa con vocación de servicio, 
     ​             responsabilidad, eficiente y ágil que constituya ​
                  una herramienta de éxito y satisfacción para 
                  ​nuestros clientes."
-          />
-        </div>
-        </div>
-        <div className="d-flex justify-content-end ">
+              />
+            </div>
+          </div>
+          <div className="d-flex justify-content-end ">
 
 
-          <div className="d-flex tag " id="vtel">
+            <div className="d-flex tag " id="vtel">
 
-            <div className="vmtexto mx-5 mt-3">
-              <div className=" text-white">
-                <h3>
-                  <strong>MISION</strong>
-                </h3>
+              <div className="vmtexto mx-5 mt-3">
+                <div className=" text-white">
+                  <h3>
+                    <strong>MISION</strong>
+                  </h3>
+                </div>
+
+                <div>
+                  <p>Realizar nuestro trabajo con prontitud
+                    utilizando lo mejor de nuestro personal
+                    humano, como tecnológico, ofreciendo de
+                    esta forma la mejor de las opciones de
+                    servicio en el ramo de gestión aduanal.</p>
+                </div>
               </div>
+              <div className="bimagen d-flex align-items-center justify-content-center">
+                <figure>
+                  <img src={img6} alt="Mision" className="w-75 h-75 circulol" />
+                </figure>
 
-              <div>
-                <p>Realizar nuestro trabajo con prontitud
-                  utilizando lo mejor de nuestro personal
-                  humano, como tecnológico, ofreciendo de
-                  esta forma la mejor de las opciones de
-                  servicio en el ramo de gestión aduanal.</p>
               </div>
             </div>
-            <div className="bimagen d-flex align-items-center justify-content-center">
-              <figure>
-              <img src={img6} alt="Mision" className="w-75 h-75 circulol"  />
-              </figure>
-                
-              </div>
+
           </div>
 
         </div>
 
-        </div>
-        
       </section>
 
       <div className="servicios mx-5">
         <div className="text-center m-5">
-        <h2>
-          <strong>Nuestros Servicios</strong>
-        </h2>
+          <h2>
+            <strong>Nuestros Servicios</strong>
+          </h2>
         </div>
         <ul>
           <li>Elaboración de Ducas</li>
@@ -225,30 +219,30 @@ function App() {
         </div>
       </section>
       <div className="mb-5 m-5">
-      <Header imagen={img12} alter="señores"  />
+        <Header imagen={img12} alter="señores" />
       </div>
-      
+
       <div className="mx-5">
-       <div className="d-flex justify-content-start iconosm">
-       <Imagenp
-        titulo="Asesorías"
-        imagen={img13}
-        alter="mesa"
-        body="Asesorías en temas de comercio internacionales, 
+        <div className="d-flex justify-content-start iconosm">
+          <Imagenp
+            titulo="Asesorías"
+            imagen={img13}
+            alter="mesa"
+            body="Asesorías en temas de comercio internacionales, 
          desde antes de la ​compra hasta la entrega de las mercancías en las bodegas del ​importador o exportador. 
          Asimismo prevenimos posibles ajustes ​realizados por la Superintendencia de Administración Tributaria ​
          SAT, en materia de comercio exterior."
-        ident="asesoria"
-      />
-       </div>
-      
+            ident="asesoria"
+          />
+        </div>
 
-      <div className="d-flex justify-content-end iconosm">
-      <Imagenp
-        titulo="Transporte"
-        imagen={img14}
-        alter="camion"
-        body="Transporte terrestre, contamos con autorización de las 
+
+        <div className="d-flex justify-content-end iconosm">
+          <Imagenp
+            titulo="Transporte"
+            imagen={img14}
+            alter="camion"
+            body="Transporte terrestre, contamos con autorización de las 
          autoridades ​aduaneras para brindar el servicio de transporte aduanero. Nuestras ​unidades tienen diferentes capacidades, 
          lo que nos permite atender el ​transporte terrestre a nivel local y nacional. Las unidades son ​
          supervisadas para cumplir con nuestros estándares de calidad y ​seguridad. Estamos en Transporte terrestre, 
@@ -257,17 +251,17 @@ function App() {
          local y nacional. Las unidades son ​supervisadas para cumplir con nuestros estándares de calidad y ​seguridad. 
          Estamos en capacidad de proporcionar transporte ​terrestre.
          capacidad de proporcionar transporte ​terrestre."
-        ident="transporte"
-      />
-      </div>
+            ident="transporte"
+          />
+        </div>
 
       </div>
-      
+
 
       <section>
 
         <div className="d-flex junstfy-content-center mx-5 pb-5 separar iconosm">
-          
+
           <div className="tamaño">
             <div className="d-flex ">
               <div className="vector">
@@ -289,7 +283,7 @@ function App() {
 
               </div>
               <div className="tfondo ">
-                <img src="/src/assets/pagina amt/10.png" alt="barco"  className=""/>
+                <img src="/src/assets/pagina amt/10.png" alt="barco" className="" />
 
               </div>
             </div>
@@ -317,46 +311,46 @@ function App() {
 
 
       <section>
-      <div className="d-flex contactos cola iconosm">
-        <div className="cleft mt-5">
-          <div className="botones2 text-white m-5">
-            <h3><strong> Servicios</strong></h3>
+        <div className="d-flex contactos cola iconosm">
+          <div className="cleft mt-5">
+            <div className="botones2 text-white m-5">
+              <h3><strong> Servicios</strong></h3>
+            </div>
+            <div className="listserv">
+              <ul>
+                <li> <p>Gestión Aduanera</p></li>
+                <li> <p>Transporte</p></li>
+                <li> <p>Recolección de Documentos</p></li>
+                <li> <p>Gestiones de licencia de importación</p></li>
+              </ul>
+            </div>
           </div>
-          <div className="listserv">
-            <ul>
-              <li> <p>Gestión Aduanera</p></li>
-              <li> <p>Transporte</p></li>
-              <li> <p>Recolección de Documentos</p></li>
-              <li> <p>Gestiones de licencia de importación</p></li>
-            </ul>
+
+          <div className="mt-5">
+            <div className="botones2 text-white m-5">
+              <h3><strong>Contáctanos</strong></h3>
+            </div>
+            <div className="centrardirec">
+
+              <div className="direc">
+                <h3>
+                  <strong>Ubicación</strong>
+                </h3>
+                <p>17 Calle 7-80 Zona 13 Aurora 1</p>
+              </div>
+              <div className="direc">
+                <h3>
+                  <strong>Ventas</strong>
+                </h3>
+                <p>Tel: 3015-6817, ​5697-6272</p>
+              </div>
+
+            </div>
+
           </div>
         </div>
-
-        <div className="mt-5">
-          <div className="botones2 text-white m-5">
-            <h3><strong>Contáctanos</strong></h3>
-          </div>
-          <div className="centrardirec">
-
-          <div className="direc">
-            <h3>
-              <strong>Ubicación</strong>
-            </h3>
-            <p>17 Calle 7-80 Zona 13 Aurora 1</p>
-          </div>
-          <div className="direc">
-            <h3>
-              <strong>Ventas</strong>
-            </h3>
-            <p>Tel: 3015-6817, ​5697-6272</p>
-          </div>
-
-          </div>
-          
-        </div>
-      </div>
       </section>
-      
+
     </>
   );
 }
